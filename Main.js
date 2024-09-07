9119845218cd4599b3c4a167b16f6cb084febf1c
Alveena-Ali <163353918+Alveena-Ali@users.noreply.github.com>
2024-09-07 23:20:49 +0500

    Que 14: Guest List

---
+++ Main.js
@@ -0,0 +1,11 @@
+"use strict";
+// Que 14: Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that
+// includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, 
+// inviting them to dinner. 
+Object.defineProperty(exports, "__esModule", { value: true });
+let Guest_List = ["Zara", "Sheza", "Laiba"];
+for (let i = 0; i < Guest_List.length; i++) {
+    console.log("Dear" + " " + Guest_List[i] +
+        ",\n\nIt is our pleasure to invite you in our dinner party.\n\nThank You!\n\n");
+}
+;
